44e181ea4bbf6f911480d859fcdb738d0c5ca2f3
santhra <santhra@team.quadloops.com>
2013-12-11 11:09:14 +0530

    Added base spider for Netaporter

---
+++ styloko/spiders/netaporter.py
@@ -0,0 +1,141 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request, FormRequest
+from scrapy.contrib.loader.processor import Compose, MapCompose, Join
+from scrapy import log
+from styloko.items import StylokoProduct, StylokoProductLoader
+from styloko.utils.text import *
+from styloko.utils.tags import *
+from styloko.utils.price import price_parser
+from styloko.processors.urls import absolute_url, absolute_url_value
+from styloko.processors.color import make_colors_list
+import urlparse
+import re
+import copy
+import w3lib
+
+
+# ----------------------------------------------------------------------
+
+RETAILER_DOMAIN = 'net-a-porter.com'
+RETAILER_NAME = 'netaporter'
+RETAILER_CODE = 'netaporter'
+
+class NetaporterSpiderProduct(ScrunchProduct):
+    def __init__(self, *args, **kwargs):
+        super(NetaporterSpiderProduct, self).__init__(*args, **kwargs)
+        self["retailer"] = RETAILER_CODE
+
+class NetaporterSpider(BaseSpider):
+    name = RETAILER_NAME
+    allowed_domains = ['net-a-porter.com']
+    HOMEPAGE = 'http://www.net-a-porter.com/'
+    ALL_DESIGNERS_PAGE = 'http://www.net-a-porter.com/designers'
+    DEBUG_CHECK_CONSTRAINTS = True
+
+    start_urls = [
+        ALL_DESIGNERS_PAGE,
+    ]
+
+    # --- fetch all category or brand pages ----------------------------
+    XPATH_CATEGORY_PAGES = """
+        //ul[@id="nav"]
+            /li[@class="category"]
+                //a/@href
+    """
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        for category_url in hxs.select(self.XPATH_CATEGORY_PAGES).extract():
+            yield Request(
+                urlparse.urljoin(response.url, category_url),
+                callback=self.parse_category_page)
+
+
+    # --- from each category page, fetch all product page links --------
+    # this would be the XPath expression to loop on product TD cells or DIVs
+    XPATH_PRODUCT_LIST_URL = """
+        //div[@id="product-list"]
+            //div[@class="product"]
+                //a[@class="product-url"]/@href
+    """
+    XPATH_NEXT_PAGES = """
+        //div[@id="pagination"]
+            //a[@title="Next"]/@href
+    """
+
+    def parse_category_page(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        for product_url in hxs.select(self.XPATH_PRODUCT_LIST_URL).extract():
+            yield Request(
+                urlparse.urljoin(response.url, product_url),
+                callback=self.parse_product_page)
+
+        for next_page_url in hxs.select(self.XPATH_NEXT_PAGES).extract():
+            yield Request(
+                urlparse.urljoin(response.url, next_page_url),
+                callback=self.parse_category_page)
+
+
+    # --- extract product items from product details page --------------
+    XPATH_PRODUCT_DETAIL = """
+        //div[@id="product-details"]
+    """
+    XPATH_PRODUCT_NAME = './/span[@class="product-title"]//text()'
+    XPATH_PRODUCT_BRAND = './/span[@class="product-brand"]//text()'
+    XPATH_PRODUCT_CATEGORIES = './/span[@class="product-category"]//text()'
+    XPATH_PRODUCT_COLORS = './/span[@class="product-color"]//text()'
+    #FIX ME : Add Xpath of SKU
+    XPATH_PRODUCT_SKU = ''
+    XPATH_PRODUCT_IMAGE_URLS = './/img[@class="product-image"]/@src'
+
+    XPATH_PRODUCT_CURRENT_PRICE = './/span[@class="product-price"]//text()'
+    XPATH_PRODUCT_PREVIOUS_PRICE = './/span[@class="product-price-discount"]//text()'
+
+    XPATH_PRODUCT_DESCRIPTION = './/span[@class="product-description"]//text()'
+    XPATH_PRODUCT_REFERENCE_NUMBER = '//p[@class="barcode"]/text()'
+    #FIXME: Add Xpath for Keywords / Tags 
+    XPATH_PRODUCT_KEYWORDS = ''
+    # this usually needs to be parsed
+    XPATH_PRODUCT_SIZE = './/select[@class="product-size"]/option//text()'
+
+    def parse_product_page(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        for product in hxs.select(self.XPATH_PRODUCT_DETAIL):
+
+            l = StylokoProductLoader(NetaporterSpiderProduct(), product)
+            l.context['response'] = response
+
+            #l.add_xpath('url', self.XPATH_PRODUCT_URL, absolute_url)
+            # this could also be response.url
+            l.add_value('url', response.url, absolute_url_value)
+
+            l.add_xpath('name', self.XPATH_PRODUCT_NAME)
+            l.add_xpath('brand', self.XPATH_PRODUCT_BRAND)
+            l.add_xpath('categories', self.XPATH_PRODUCT_CATEGORIES,
+                MapCompose(Join(u', '), unicode.lower))
+            l.add_xpath('reference_number',self.XPATH_PRODUCT_REFERENCE_NUMBER)
+            l.add_xpath('sku',self.XPATH_PRODUCT_SKU)
+            l.add_xpath('description', self.XPATH_PRODUCT_DESCRIPTION,
+                Join(u"\n"),
+                unicode.strip)
+
+            l.add_xpath('images', self.XPATH_PRODUCT_IMAGE_URLS,
+                absolute_url)
+
+            l.add_xpath('previous_price', self.XPATH_PRODUCT_CURRENT_PRICE,price_parser)
+            l.add_xpath('current_price', self.XPATH_PRODUCT_PREVIOUS_PRICE,
+                price_parser)
+
+            l.add_xpath('keywords',self.XPATH_PRODUCT_KEYWORDS)
+            # remember to add other color variants as another item
+            l.add_xpath('colors', self.XPATH_PRODUCT_COLORS)
+
+            l.add_xpath('sizes', self.XPATH_PRODUCT_SIZE)
+
+            item = l.load_item(check=self.DEBUG_CHECK_CONSTRAINTS)
+
+            yield item
